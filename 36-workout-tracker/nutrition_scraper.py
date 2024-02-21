56ae16f9a50d81a6fba3fa015cc3cfc678efb79a
Xavier Alexander <xalexander51@gmail.com>
2024-02-21 05:14:17 -0500

    Create NutritionixAPI class

---
+++ 36-workout-tracker/nutrition_scraper.py
@@ -0,0 +1,29 @@
+import requests
+import os
+
+
+class NutritionixAPI():
+    def __init__(self):
+        self.url = 'https://trackapi.nutritionix.com/v2/natural/exercise'
+
+    def exercise_estimator(self, user_input):
+        """
+       Args:
+           user_input: Details about the exercise
+
+       Returns:
+           Response with information about the exercise
+       """
+
+        auth_headers = {
+            'Content-Type': 'application/json',
+            'x-app-id': os.environ['NUTRITIONIX_ID'],
+            'x-app-key': os.environ['NUTRITIONIX_KEY']
+        }
+
+        body = {
+            'query': user_input
+        }
+
+        response = requests.post(url=self.url, headers=auth_headers, json=body)
+        print(response.text)
