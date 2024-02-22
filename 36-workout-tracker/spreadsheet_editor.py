4e03222957eaa296aae77098f2c7b8eaccef1455
Xavier Alexander <xalexander51@gmail.com>
2024-02-22 05:34:14 -0500

    Add insert method

--- 36-workout-tracker/spreadsheet_editor.py
+++ 36-workout-tracker/spreadsheet_editor.py
@@ -6,3 +6,22 @@ class SpreadsheetEditor():
     def __init__(self):
         self.base_url = 'https://api.sheety.co/'
         self.spreadsheet_info = os.environ['SPREADSHEET_INFO']
+        self.sheety_token = os.environ['SHEETY_TOKEN']
+
+    def insert(self):
+        headers = {
+            'Authorization': 'Bearer ' + self.sheety_token
+        }
+        payload = {
+            'workout': {
+                'date': 'xavier',
+                'time': '22',
+                'exercise': 'test',
+                'duration': 'test',
+                'calories': 'test'
+
+            }
+        }
+        response = requests.post(url=self.base_url + self.spreadsheet_info, headers=headers, json=payload)
+
+        print(response.text)
