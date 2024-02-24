844c1c55f3dbef9ecf33ea782a69345fbd84dd32
Xavier Alexander <xalexander51@gmail.com>
2024-02-24 05:21:03 -0500

    Add time converter method

--- 36-workout-tracker/spreadsheet_editor.py
+++ 36-workout-tracker/spreadsheet_editor.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from datetime import date
 
 
 class SpreadsheetEditor():
@@ -7,16 +8,32 @@ class SpreadsheetEditor():
         self.base_url = 'https://api.sheety.co/'
         self.spreadsheet_info = os.environ['SPREADSHEET_INFO']
         self.sheety_token = os.environ['SHEETY_TOKEN']
+        self.today = date.today()
+        self.exercise = ''
+        self.duration = ''
 
-    def insert(self):
+    @staticmethod
+    def time_converter(time):
+        hours = int(time) // 60
+        minutes = int(time) % 60
+        if time > 60:
+            return f'{hours}:{minutes}:00'
+        else:
+            return f'{minutes}:00.00'
+
+    def insert(self, data):
         headers = {
             'Authorization': 'Bearer ' + self.sheety_token
         }
+        for i in data['exercises']:
+            self.exercise = i['name']
+            self.duration = i['duration_min']
+
         payload = {
             'workout': {
-                'date': 'xavier',
-                'time': '22',
-                'exercise': 'test',
+                'date': self.today,
+                'time': self.time_converter(self.duration),
+                'exercise': self.exercise,
                 'duration': 'test',
                 'calories': 'test'
 
