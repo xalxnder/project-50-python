ab9a3e35f3cfb65f6deeccbe9199c2b1cd57278b
Xavier Alexander <xalexander51@gmail.com>
2024-02-26 19:39:19 -0500

    Remove comments

--- 36-workout-tracker/main.py
+++ 36-workout-tracker/main.py
@@ -7,8 +7,7 @@ spreadsheet_editor = SpreadsheetEditor()
 user_exercise = input('What exercise did you complete today? ')
 exercise_data = nutritionix_api.get_exercise_data(user_exercise)
 
-# To do
-# Pass response from exercise estimator into spreadsheet editor
-# spreadsheet_editor.insert()
+
+spreadsheet_editor.insert(exercise_data)
 
 
