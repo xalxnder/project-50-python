c2d8f6a5a51855eb1ad5306fdb4d4be1a95041e1
Xavier Alexander <xalexander51@gmail.com>
2024-02-26 19:39:49 -0500

    Add docstrings

--- 36-workout-tracker/spreadsheet_editor.py
+++ 36-workout-tracker/spreadsheet_editor.py
@@ -1,6 +1,6 @@
 import requests
 import os
-from datetime import date
+from datetime import date, datetime
 
 
 class SpreadsheetEditor():
@@ -11,34 +11,54 @@ class SpreadsheetEditor():
         self.today = date.today()
         self.exercise = ''
         self.duration = ''
+        self.calories = ''
 
     @staticmethod
     def time_converter(time):
+        """
+
+        Args:
+            time: Exercise duration in minutes
+
+        Returns:
+            Time formatted as mm:ss or hh:mm:ss
+
+        """
         hours = int(time) // 60
         minutes = int(time) % 60
         if time > 60:
-            return f'{hours}:{minutes}:00'
+            return f'{hours}:{minutes:02}:00'
         else:
-            return f'{minutes}:00.00'
+            return f'{minutes}:00'
 
     def insert(self, data):
+        """
+        Args:
+            data: Exercise data returned from the nutritionix_api
+
+        Returns:
+            Returns response from sheety API
+        """
+        print(len(data))
         headers = {
             'Authorization': 'Bearer ' + self.sheety_token
         }
         for i in data['exercises']:
             self.exercise = i['name']
             self.duration = i['duration_min']
+            self.calories = i['nf_calories']
 
-        payload = {
-            'workout': {
-                'date': self.today,
-                'time': self.time_converter(self.duration),
-                'exercise': self.exercise,
-                'duration': 'test',
-                'calories': 'test'
+            payload = {
+                'workout': {
+                    'date': self.today.strftime('%m/%d/%Y'),
+                    'time': self.time_converter(self.duration),
+                    'exercise': self.exercise,
+                    'duration': self.time_converter(self.duration),
+                    'calories': self.calories
 
+                }
             }
-        }
-        response = requests.post(url=self.base_url + self.spreadsheet_info, headers=headers, json=payload)
 
-        print(response.text)
+            response = requests.post(url=self.base_url + self.spreadsheet_info, headers=headers, json=payload)
+
+        return response
