ee5952b5c69844d61ac6e9c82c02d11fcbdcd24d
Xavier Alexander <xalexander51@gmail.com>
2024-03-02 04:54:34 -0500

    Testing

--- 37-flight-price-tracker/main.py
+++ 37-flight-price-tracker/main.py
@@ -8,11 +8,13 @@ spreadsheet_editor = SpreadsheetEditor()
 spreadhseet_data = spreadsheet_editor.read()
 
 
-for i in spreadhseet_data['prices']:
-    city_column = i['city']
-    code_column = i['iataCode']
-    row_column = i['id']
-    if code_column == '':
-        iata_code = flight_checker.get_code(city_column)
-        spreadsheet_editor.update_iata_codes(row_column, iata_code)
+# for i in spreadhseet_data['prices']:
+#     city_column = i['city']
+#     code_column = i['iataCode']
+#     row_column = i['id']
+#     if code_column == '':
+#         iata_code = flight_checker.get_code(city_column)
+#         spreadsheet_editor.update_iata_codes(row_column, iata_code)
+
+flight_checker.get_prices()
 
