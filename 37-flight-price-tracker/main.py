3cd97abb54aaab37424dab8bc52496fbc9b1922e
Xavier Alexander <xalexander51@gmail.com>
2024-03-08 04:39:51 -0500

    Cleanup and refactor

--- 37-flight-price-tracker/main.py
+++ 37-flight-price-tracker/main.py
@@ -1,32 +1,35 @@
 from flight_checker import *
-from spreadsheet_editor import *
+from spreadsheet_interface import *
 from datetime import date, timedelta
 from message_sender import *
 
 flight_checker = FlightChecker()
 
-spreadsheet_editor = SpreadsheetEditor()
-spreadhseet_data = spreadsheet_editor.read()
+spreadsheet_interface = SpreadsheetInterface()
+PRICES_SPREADSHEET = spreadsheet_interface.read('prices')
+USERS_SPREADSHEET = spreadsheet_interface.read('users')
+
+
 message_sender = MessageSender()
 
 TODAY = date.today().strftime("%d/%m/%Y")
 SIX_MONTHS = (date.today() + timedelta(days=6 * 30)).strftime("%d/%m/%Y")
-FLIGHTS_SPREADSHEET = spreadhseet_data['prices']
 ORIGIN_IATA = 'PHL'
 
-# for i in spreadhseet_data['prices']:
-#     city_column = i['city']
-#     code_column = i['iataCode']
-#     row_column = i['id']
-#     if code_column == '':
-#         iata_code = flight_checker.get_code(city_column)
-#         spreadsheet_editor.update_iata_codes(row_column, iata_code)
+for i in PRICES_SPREADSHEET:
+    print(i)
+    # city_column = i['city']
+    # code_column = i['iataCode']
+    # row_column = i['id']
+    # if code_column == '':
+    #     iata_code = flight_checker.get_code(city_column)
+    #     spreadsheet_interface.update_iata_codes(row_column, iata_code)
 
 
-for cell in FLIGHTS_SPREADSHEET:
+for cell in PRICES_SPREADSHEET:
     flight_checker.get_prices(ORIGIN_IATA, cell['iataCode'], TODAY, SIX_MONTHS)
 
-for cell in FLIGHTS_SPREADSHEET:
+for cell in PRICES_SPREADSHEET:
     spreadsheet_price = cell['lowestPrice']
     try:
         new_price = flight_checker.prices[cell['iataCode']]
@@ -36,4 +39,5 @@ for cell in FLIGHTS_SPREADSHEET:
         if new_price < spreadsheet_price:
             message_sender.message += f'Only ${new_price} to fly from {ORIGIN_IATA} to {cell["iataCode"]}\n'
 
-message_sender.send_message()
+print(message_sender.message)
+# message_sender.send_message()
