56e85153976e95cf37578a8b76583b736d459586
Xavier Alexander <xalexander51@gmail.com>
2024-03-13 04:28:06 -0400

    Update method

--- 39-spotify-playlist-creator/song_scraper.py
+++ 39-spotify-playlist-creator/song_scraper.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import re
-
+import pandas as pd
 
 
 class SongScraper:
@@ -10,15 +10,25 @@ class SongScraper:
         self.response = requests.get(self.BILLBOARD_URL)
         self.website = BeautifulSoup(self.response.text, "html.parser")
 
+    def get_song_info(self):
+        """
+        Args:
+            self: An instance of the SongScraper class.
+        Returns:
+            list: A list of dictionaries containing the Artists and Songs.
 
-    def get_artist_name(self):
+
+        """
         artist_names = [artist.text.strip() for artist in self.website.find_all(name="span",
-                                                                                     class_="c-label a-no-trucate a-font-primary-s lrv-u-font-size-14@mobile-max u-line-height-normal@mobile-max u-letter-spacing-0021 lrv-u-display-block a-truncate-ellipsis-2line u-max-width-330 u-max-width-230@tablet-only")]
-        return artist_names
 
-    def get_song_title(self):
+                                                                                class_="c-label a-no-trucate a-font-primary-s lrv-u-font-size-14@mobile-max u-line-height-normal@mobile-max u-letter-spacing-0021 lrv-u-display-block a-truncate-ellipsis-2line u-max-width-330 u-max-width-230@tablet-only")]
         song_titles = [song.text.strip() for song in self.website.find_all(name="h3",
-                                                                     class_="c-title a-no-trucate a-font-primary-bold-s u-letter-spacing-0021 lrv-u-font-size-18@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis u-max-width-330 u-max-width-230@tablet-only")]
+                                                                           class_="c-title a-no-trucate a-font-primary-bold-s u-letter-spacing-0021 lrv-u-font-size-18@tablet lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max a-truncate-ellipsis u-max-width-330 u-max-width-230@tablet-only")]
 
-        return song_titles
+        song_info = pd.DataFrame({
+                                           'Artist': artist_names,
+                                           'Song': song_titles})
+        song_info = song_info.to_dict(orient='records')
+        return song_info
+
 
