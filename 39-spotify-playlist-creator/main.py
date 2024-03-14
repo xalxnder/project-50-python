a872139311349a1e0722786dad5f579be93432af
Xavier Alexander <xalexander51@gmail.com>
2024-03-14 04:36:43 -0400

    Update

--- 39-spotify-playlist-creator/main.py
+++ 39-spotify-playlist-creator/main.py
@@ -6,6 +6,17 @@ spotify_interface = SpotifyInterface()
 song_scraper = SongScraper()
 # spotify_interface.request_authorization()
 # spotify_interface.request_access_token()
-# spotify_interface.get_tracks()
+# spotify_interface.search()
 
 song_info = song_scraper.get_song_info()
+# print(song_info[0]['Artist'])
+# print(song_info[0]['Song'])
+
+# spotify_interface.get_song_uri(song_info[0]['Artist'], song_info[0]['Song'])
+# track_uris = [spotify_interface.get_song_uri(song['Artist'], song['Song']) for song in song_info]
+track_uris = [spotify_interface.get_song_uri(song['Artist'], song['Song']) for i, song in enumerate(song_info) if i < 5]
+
+print(track_uris)
+
+
+spotify_interface.update_playlist(track_uris)
