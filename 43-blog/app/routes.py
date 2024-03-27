8487a2e4dab5ef07b8e81a617b0eb254f530975c
Xavier Alexander <xalexander51@gmail.com>
2024-03-27 04:32:00 -0400

    Update

---
+++ 43-blog/app/routes.py
@@ -0,0 +1,22 @@
+from flask import render_template, redirect
+import requests
+from app import app
+
+
+@app.route('/')
+def index():
+    blog_post_data = 'https://api.npoint.io/8595ea108855569b1d32'
+    response = requests.get(blog_post_data)
+    posts = response.json()
+    return render_template('index.html', posts=posts)
+
+@app.route('/post/<int:post_id>')
+def get_post(post_id):
+    blog_post_data = 'https://api.npoint.io/8595ea108855569b1d32'
+    response = requests.get(blog_post_data)
+    posts = response.json()
+    post = None
+    for p in posts:
+        if p['id'] == post_id:
+            post = p
+    return render_template('post.html', post=post)
