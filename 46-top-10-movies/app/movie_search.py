5a98f0aa38652f94001d1ecbde1b62e1eccdc6da
Xavier Alexander <xalexander51@gmail.com>
2024-05-08 04:31:49 -0400

    Clean up variable name

--- 46-top-10-movies/app/movie_search.py
+++ 46-top-10-movies/app/movie_search.py
@@ -1,7 +1,8 @@
 import requests
 import os
 
-class MovieAPI:
+
+class MovieAPI():
     def __init__(self):
         self.API_KEY = os.environ['MOVIE_API_TOKEN']
 
@@ -19,11 +20,11 @@ class MovieAPI:
         response = requests.get(url, headers=headers, params=parameters).json()
         results = response['results']
         cleaned_up_results = [{
-                                  'title': x['original_title'],
-                                  'image_url': f'https://image.tmdb.org/t/p/w500/{x["poster_path"]}',
-                                'year': x['release_date'].split('-')[0],
-                                'description': x['overview']
-        } for x in results]
+            'title': movie['original_title'],
+            'image_url': f'https://image.tmdb.org/t/p/w500/{movie["poster_path"]}',
+            'year': movie['release_date'].split('-')[0],
+            'description': movie['overview'],
+            'id': movie['id']
+        } for movie in results]
+        print(cleaned_up_results)
         return cleaned_up_results
-
-
